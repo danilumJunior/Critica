5048fd5f82c988fca0cb4f24b9c78bcba42f5052
Danilo <danilojunior2016@outlook.com>
2026-05-10 06:02:47 -0300

    Criação de usuario padrao e admin, junto com sistema de login

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,110 @@
 import streamlit as st
 from supabase import create_client
 
+# -----------------------------------
+# CONEXÃO SUPABASE
+# -----------------------------------
 
 url = st.secrets["SUPABASE_URL"]
 key = st.secrets["SUPABASE_ANON_KEY"]
 
 supabase = create_client(url, key)
 
-st.title("Sistema de login")
+# -----------------------------------
+# TÍTULO
+# -----------------------------------
 
-email = st.text_input("Email")
-senha = st.text_input("Senha", type="password")
+st.title("Sistema de Login")
 
-col1, col2 = st.columns(2)
+# -----------------------------------
+# SE USUÁRIO NÃO ESTIVER LOGADO
+# -----------------------------------
 
-with col1:
+if "usuario_id" not in st.session_state:
 
-    if st.button("Cadastrar"):
-        resposta = supabase.auth.sign_up({"email":email, "password":senha})
-        st.success("Usuário cadastrado com sucesso!")
+    email = st.text_input("E-mail")
 
-with col2:
+    senha = st.text_input(
+        "Senha",
+        type="password"
+    )
 
-     if st.button("Login"):
+    col1, col2 = st.columns(2)
 
-        resposta = supabase.auth.sign_in_with_password({
-            "email": email,
-            "password": senha
-        })
+    # CADASTRO
+    with col1:
 
-        st.success("Login realizado!")
+        if st.button("Cadastrar"):
+
+            resposta = supabase.auth.sign_up({
+                "email": email,
+                "password": senha
+            })
+
+            st.success("Usuário cadastrado!")
+
+    # LOGIN
+    with col2:
+
+        if st.button("Login"):
+
+            resposta = supabase.auth.sign_in_with_password({
+                "email": email,
+                "password": senha
+            })
+
+            st.session_state["usuario_id"] = resposta.user.id
+
+            st.session_state["usuario_email"] = resposta.user.email
+
+            st.rerun()
+
+# -----------------------------------
+# SE USUÁRIO ESTIVER LOGADO
+# -----------------------------------
+
+else:
+
+    usuario_id = st.session_state["usuario_id"]
+
+    usuario_email = st.session_state["usuario_email"]
+
+    st.success("Login realizado!")
+
+    st.write("ID:", usuario_id)
+
+    st.write("EMAIL:", usuario_email)
+
+    # BUSCA PROFILE
+    dados_profile = supabase.table("profiles") \
+        .select("*") \
+        .eq("id", usuario_id) \
+        .execute()
+
+    st.write(dados_profile.data)
+
+    profile = dados_profile.data[0]
+
+    tipo_usuario = profile["tipo"]
+
+    st.write("TIPO DE USUÁRIO:", tipo_usuario)
+
+    # ADMIN
+    if tipo_usuario == "Admin":
+
+        st.write("Bem-vindo, admin!")
+
+        st.header("Área de Administração")
+
+        st.button("Criar Obra")
+        st.button("Editar Obra")
+        st.button("Excluir Obra")
+
+    # PADRÃO
+    else:
+
+        st.write("Bem-vindo, usuário!")
+
+        st.header("Área do Usuário")
+
+        st.write("Aqui você pode visualizar as obras disponíveis.")
